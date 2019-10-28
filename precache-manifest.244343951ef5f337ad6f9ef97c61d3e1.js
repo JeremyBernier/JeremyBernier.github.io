28ff91a170b098207579f8295cc2991a598d6af9
Jeremy Bernier <jeremy.s.bernier@gmail.com>
2019-10-29 03:10:22 +0900

    icons

---
+++ precache-manifest.244343951ef5f337ad6f9ef97c61d3e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f4c99f8dee0180d0df1",
+    "url": "1.js"
+  },
+  {
+    "revision": "d3a27c0319722dcfe5ec",
+    "url": "2.js"
+  },
+  {
+    "revision": "108c1396e8cdace97241",
+    "url": "3.js"
+  },
+  {
+    "revision": "fedce34db9689172d614a05034fca695",
+    "url": "img/portrait.jpg"
+  },
+  {
+    "revision": "042fb5b5016f8aa182b583e7b274302f",
+    "url": "index.html"
+  },
+  {
+    "revision": "cbcb230d93087a7310f8",
+    "url": "main.js"
+  },
+  {
+    "revision": "acf11623baddb9d332a9e218b8fdcb62",
+    "url": "manifest.json"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("precache-manifest.89c544e01b99ea7f682e8c46c208606f.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("precache-manifest.244343951ef5f337ad6f9ef97c61d3e1.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 workbox.precaching.precacheAndRoute(self.__precacheManifest)
 

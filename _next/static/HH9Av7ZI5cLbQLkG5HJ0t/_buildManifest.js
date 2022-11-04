707b3748a5a9ef29e91304806d0fac1421cc99f5
Jeremy Bernier <jeremy.s.bernier@gmail.com>
2022-11-04 16:06:44 +0100

    cool

---
+++ _next/static/HH9Av7ZI5cLbQLkG5HJ0t/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/294-26d84beca2547e31.js","static/chunks/pages/index-7a89717800b6d6c9.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
